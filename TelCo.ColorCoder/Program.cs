3c0783bc643b135679e7432cf407bd6839683354
Suarez Plata Daniel (RBEI/EBS11-SX) <Daniel.SuarezPlata@mx.bosch.com>
2022-01-10 16:28:30 -0600

    Delete commented code

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -32,73 +32,6 @@ namespace TelCo.ColorCoder
             colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
         }
 
-        ///// <summary>
-        ///// Given a pair number function returns the major and minor colors in that order
-        ///// </summary>
-        ///// <param name="pairNumber">Pair number of the color to be fetched</param>
-        ///// <returns></returns>
-        //private static ColorPair GetColorFromPairNumber(int pairNumber)
-        //{
-        //    // The function supports only 1 based index. Pair numbers valid are from 1 to 25
-        //    int minorSize = colorMapMinor.Length;
-        //    int majorSize = colorMapMajor.Length;
-        //    if (pairNumber < 1 || pairNumber > minorSize * majorSize)
-        //    {
-        //        throw new ArgumentOutOfRangeException(
-        //            string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
-        //    }
-            
-        //    // Find index of major and minor color from pair number
-        //    int zeroBasedPairNumber = pairNumber - 1;
-        //    int majorIndex = zeroBasedPairNumber / minorSize;
-        //    int minorIndex = zeroBasedPairNumber % minorSize;
-
-        //    // Construct the return val from the arrays
-        //    ColorPair pair = new ColorPair() { majorColor = colorMapMajor[majorIndex],
-        //        minorColor = colorMapMinor[minorIndex] };
-            
-        //    // return the value
-        //    return pair;
-        //}
-        /// <summary>
-        /// Given the two colors the function returns the pair number corresponding to them
-        /// </summary>
-        /// <param name="pair">Color pair with major and minor color</param>
-        /// <returns></returns>
-        private static int GetPairNumberFromColor(ColorPair pair)
-        {
-            // Find the major color in the array and get the index
-            int majorIndex = -1;
-            for (int i = 0; i < colorMapMajor.Length; i++)
-            {
-                if (colorMapMajor[i] == pair.majorColor)
-                {
-                    majorIndex = i;
-                    break;
-                }
-            }
-
-            // Find the minor color in the array and get the index
-            int minorIndex = -1;
-            for (int i = 0; i < colorMapMinor.Length; i++)
-            {
-                if (colorMapMinor[i] == pair.minorColor)
-                {
-                    minorIndex = i;
-                    break;
-                }
-            }
-            // If colors can not be found throw an exception
-            if (majorIndex == -1 || minorIndex == -1)
-            {
-                throw new ArgumentException(
-                    string.Format("Unknown Colors: {0}", pair.ToString()));
-            }
-
-            // Compute pair number and Return  
-            // (Note: +1 in compute is because pair number is 1 based, not zero)
-            return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
-        }
         /// <summary>
         /// Test code for the class
         /// </summary>
@@ -124,12 +57,12 @@ namespace TelCo.ColorCoder
             Debug.Assert(testPair1.minorColor == Color.Green);
 
             ColorPair testPair2 = new ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
-            pairNumber = Program.GetPairNumberFromColor(testPair2);
+            pairNumber = Program.FetchPairNummber(testPair2);
             Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
             Debug.Assert(pairNumber == 18);
 
             testPair2 = new ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
-            pairNumber = Program.GetPairNumberFromColor(testPair2);
+            pairNumber = Program.FetchPairNummber(testPair2);
             Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}", testPair2, pairNumber);
             Debug.Assert(pairNumber == 6);
         }
